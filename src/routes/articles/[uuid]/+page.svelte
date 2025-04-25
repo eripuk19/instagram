395037d21daf8c9724877a32c2c08b04390a411f
eripuk19 <eripuk19@htl-shkoder.com>
2025-04-25 10:27:13 +0200

    method to upvote an article

--- src/routes/articles/[uuid]/+page.svelte
+++ src/routes/articles/[uuid]/+page.svelte
@@ -10,6 +10,12 @@
     <div class="border border-gray-300 p-4 my-2 rounded-md shadow-sm flex justify-between items-center">
         <img src="{article.image}" alt="uploaded" class="w-114"/>
       <p class="text-gray-800 font-medium">{article.author} - {article.description} </p>
+      <form action="?/upvoteImage" method="POST" use:enhance>
+        <input type="hidden" name="id" value={article.id} />
+        <button type="submit" aria-label="like" class="w-11 h-10 hover:scale-110 transition-transform duration-200">
+            <img src="https://99cjakqnnrlcgkh9.public.blob.vercel-storage.com/projekt/upvote-5lecHC1UpTX9MIBrSFk1jcb6yabUZj.png" alt="upvote" />
+        </button>
+    </form>
       <p class="text-gray-800 font-medium">Likes: {article.votes} </p>
 
     </div>
